2e91b434bc44a705bec44308d04d07e0bf57e33c
hxsxixn <100697095+hxsxixn@users.noreply.github.com>
2022-09-30 04:07:29 +0800

    update

--- script/ui.js
+++ script/ui.js
@@ -1,6 +1,16 @@
 
 const codeRunner = {};
 
+let html_code = $("#html-code").val();
+let css_code = $("#css-code").val();
+let js_code = $("#js-code").val();
+
+codeRunner.getCode = () => {
+    html_code = $("#html-code").val();
+    css_code = $("#css-code").val();
+    js_code = $("#js-code").val();
+}
+
 codeRunner.syncTab = () => {
     $("h5").click(function(){
         $(".code-container").css("width","0");
@@ -11,10 +21,33 @@ codeRunner.syncTab = () => {
 }
 
 codeRunner.sample = () => {
-    $("#sample").click(function(){
+    $("#sample").on('click',function(){
         jQuery.get('../demo_html.txt',function(data){$("#html-code").val(data);});
         jQuery.get('../demo_css.txt',function(data){$("#css-code").val(data);});
-        jQuery.get('../demo_js.txt',function(data){$("#js-code").val(data);$(document).ready(run());});
+        jQuery.get('../demo_js.txt',function(data){
+            const ifram = $(document.getElementsByTagName('iframe')[0].contentWindow);
+            $("#js-code").val(data);
+            $(ifram).ready(codeRunner.run());
+        });
+    })
+}
+
+codeRunner.new = () => {
+    $("#new-btn").on('click',function(){
+        jQuery.get('../single/index.html',function(data){
+            $('main').html(data);
+            const swiper = new Swiper('.swiper',{
+                pagination: {el:'.swiper-pagination'},
+                loop: true,
+                slidesPerview: 'auto',
+                speed: 1000,
+                autoplay:{
+                  delay: 2000,
+                  disableOnInteraction: false,
+                },
+              })
+              console.log('swiper init')
+        });
     })
 }
 
@@ -37,32 +70,31 @@ codeRunner.saveFile = (deta,filename) => {
 }
 
 codeRunner.run = () => {
-    let html_code = $("#html-code").val();
-    let css_code = $("#css-code").val();
-    let js_code = $("#js-code").val();
-    $("#result").contents().find("style").html(css_code);
+    codeRunner.getCode();
     $("#result").contents().find("main").html(html_code);
+    $("#result").contents().find("style").html(css_code);
     $("#result").contents().find("script").next("script").html(js_code);
 }
 
 codeRunner.eventListner = function(){
     const self = this;
-    $("#run-btn").click(function(){
+
+    self.sample();
+    self.new();
+
+    $("#run-btn").on('click',function(){
         self.run();
     })
     
-    $("#clear").click(function(){
+    $("#clear").on('click',function(){
+        $('main').text('');
         $("#html-code,#css-code,#js-code").val("");
         self.run();
     })
 
-    $("#save-btn").click(function(){
+    $("#save-btn").on('click',function(){
         html_code = $("#result").val();
-        // css_code = $("#css-code").val();
-        // js_code = $("#js-code").val();
         self.saveFile(html_code,"html_code.text");
-        // saveFile_html(css_code,"css_code.css");
-        // saveFile_html(js_code,"js_code.js");
     })
 }
 
@@ -71,6 +103,9 @@ codeRunner.init = function(){
     this.eventListner();
 }
 
+
+
 $(()=>{
     codeRunner.init();
 })
+
